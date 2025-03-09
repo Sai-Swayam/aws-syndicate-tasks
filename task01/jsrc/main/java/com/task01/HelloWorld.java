1cd58872e463eec3c7f863f759005975548a1ae4
Sai Swayam Shree <saiswayam_shree@epam.com>
2025-03-09 20:34:18 +0530

    patch 1.1

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -5,7 +5,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @LambdaHandler(
@@ -19,7 +19,7 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
+		Map<String, Object> resultMap = new LinkedHashMap<>();
 		resultMap.put("statusCode", 200);
 		resultMap.put("body", "Hello from Lambda");
 		return resultMap;
